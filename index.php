80231909754f932b16220be24d4d36acd0513d56
Matheus Dias de Souza <111648189+Mdias733@users.noreply.github.com>
2023-03-25 17:21:02 -0300

    Create index.php

---
+++ index.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Registro</title>
+    <link rel="stylesheet" href="styles.css">
+
+
+    <style>
+        body{
+            font-family: Arial, Helvetica, sans-serif;
+            background-image: linear-gradient(to right, rgb(20, 147, 220), rgb(17, 54, 121));
+        }
+    </style>
+</head>
+<body>
+<form action="CRUD/cad_user.php" method="post">
+    <div class="box">
+        <form action="">
+            <fieldset>
+                <h3>CADASTRO DE USUÁRIO</h3>
+                <br><br><br>
+                <h3 colo><a href="cadastro.php" style="color: rgb(255,255,255)">Sing Up</a></h3> <br><br><br> <h3 colo><a href="tabela.php" style="color: rgb(255,255,255)">Vizualizar Usuários</a></h3>
+            </fieldset>
+            
+        </form>
+    </div>
+    
+</body>
+</html>
